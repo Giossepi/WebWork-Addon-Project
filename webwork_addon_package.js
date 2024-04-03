dcebd388c7e8a3de0cfb4f55aaac11f4f09c08d8
Joseph Windmann <97253787+Giossepi@users.noreply.github.com>
2024-04-03 12:31:55 -0600

    [CC][FI] unified case | moved away from style | changed to rem
    *Feature Improvement
    [CC]
        Unified name case of all functions to snake_case (it's easier to read)
    [FI]
        Changed from percent width to rem so the user can make it as large as they want, also added z-index so it should go over all things on page
        Moved style tags from live preview to own css rule

--- webwork_addon_package.js
+++ webwork_addon_package.js
@@ -2,11 +2,11 @@
 let answer_input = document.getElementsByClassName("codeshard")
 let stylesheet = document.styleSheets[0]
 let last_clicked = "init"
-let new_width = 50
-// add a black border to the bigger boxes so you know which ones are on and off (j/w)
-stylesheet.insertRule(`.bigger { width: ${new_width}%; border: 1px solid black !important}`, 0);
+let new_width = 30
+stylesheet.insertRule(`.bigger {width: ${new_width}rem; border: 1px solid black !important; z-index: 100000}`, 0);
+stylesheet.insertRule(`#live-preview {border: 2px solid red; padding: 5px}`, 1);
 
-function toggleBiggerClassAll(){
+function toggle_bigger_class_all(){
     total_bigger = 0
     for(let div of answer_input){
         if(div.className.includes("bigger")){
@@ -24,23 +24,23 @@ function toggleBiggerClassAll(){
     }
 }
 
-function sizeChanger(direction){
-    if(direction == "up" && new_width <= 95){
-        new_width += 5
-    }else if(direction == "down" && new_width >= 10){
-        new_width -= 5
+function size_changer(direction){
+    if(direction == "up"){
+        new_width += 3
+    }else if(direction == "down" && new_width >= 6){
+        new_width -= 3
     }
     stylesheet.deleteRule(0)
-    stylesheet.insertRule(`.bigger { width: ${new_width}%;}`, 0);
-    console.log(`The new width is ${new_width} percent`)
+    stylesheet.insertRule(`.bigger {width: ${new_width}rem; border: 1px solid black !important; z-index: 100000}`, 0);
+    console.log(`The new width is ${new_width} rem`)
 }
 
-function toggleBiggerClassById(){
+function toggle_bigger_class_by_id(){
     let target = document.getElementById(last_clicked)
     target.classList.toggle("bigger")
 }
 
-function setLastClickedToId(e){
+function set_last_clicked_to_id(e){
     let target_classes = e.target.className
     if(target_classes.includes("codeshard")){
         if(e.target.id != ""){
@@ -59,7 +59,6 @@ function spawn_preview(value){
     let parent = document.getElementById(last_clicked).parentElement
     let spawned_preview = document.getElementById("output_problem_body").appendChild(document.createElement("div"))
     spawned_preview.setAttribute("id", "live-preview")
-    spawned_preview.setAttribute("style", "border: 2px solid red; padding: 5px")
     spawned_preview.textContent = "`" + value + "`"
 }
 
@@ -99,16 +98,16 @@ for(let input of answer_input){
 document.addEventListener("keydown", function(e){
     if(e.key == "~"){
         e.preventDefault()
-        toggleBiggerClassAll()
+        toggle_bigger_class_all()
     }else if(e.key == "|" && last_clicked != "init"){
         e.preventDefault()
-        toggleBiggerClassById()
+        toggle_bigger_class_by_id()
     }else if(e.key == "PageUp"){
         e.preventDefault()
-        sizeChanger("up")
+        size_changer("up")
     }else if(e.key == "PageDown"){
         e.preventDefault()
-        sizeChanger("down")
+        size_changer("down")
     }
 })
 // you cannot capture print screen on keydown (j/w)
@@ -121,7 +120,7 @@ document.addEventListener("keyup", function(e){
 })
 
 document.addEventListener("click", function(e){
-    setLastClickedToId(e)
+    set_last_clicked_to_id(e)
 })
 
 console.log("WebWork Addon Package Loaded! WAP is provided as is with no warranty or support, made by Windmann J")
